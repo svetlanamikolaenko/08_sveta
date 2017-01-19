9aeeeb8603643e613da7876d4874d56e79f226a2
svetlanamikolaenko <Svetlana.mikolaenko@gmail.com>
2017-01-19 12:21:13 +0200

    Update 08_sveta.py

--- 08_sveta.py
+++ 08_sveta.py
@@ -1,7 +1,13 @@
 import random
-name = input("What would you like?")
+name = input("What would you like? ")
 dishes_list = name.split(',')
-dishes_list =[x.strip() for x in dishes_list]
-dish_dict = {a:random.randint(0,100) for a in dishes_list}
-for key, value in dish_dict.items():
-    print ("%s \t \t %s" % (key, value))
+if name.strip() !=(""):
+    dishes_list =[x.strip() for x in dishes_list if x.strip()!=("")]#запамятати пусті рядки
+    if dishes_list == []:#якщо все ж таки ми ввели пусті рядки, то вивести
+        print("Input was empty")
+    else:        
+        dish_dict = {a.capitalize():random.randint(0,100) for a in dishes_list}#де а -елемент списку, в якому тип кожного елементу стрінг 
+        for key, value in dish_dict.items():
+            print ("%s \t \t %s" % (key, value))
+else:
+    print ('No input')
